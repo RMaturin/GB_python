4dcf761b24657869aee8b1239bf826e2658c9edf
RMaturin <maturin@mail.ru>
2021-01-13 19:46:48 +0300

    [upd] Закончил ДЗ урок 4

--- Les04/006.py
+++ Les04/006.py
@@ -8,3 +8,32 @@
 # Например, в первом задании выводим целые числа, начиная с 3, а при достижении числа 10 завершаем цикл.
 # Во втором также необходимо предусмотреть условие, при котором повторение элементов списка будет прекращено.
 
+from itertools import count
+from itertools import cycle
+
+
+def iter_a(start_num):
+    for el in count(start_num):
+        yield el
+
+
+def iter_b(in_list):
+    for el in cycle(in_list):
+        yield el
+
+
+i1 = iter_a(3)
+for i in i1:
+    if i > 10:
+        break
+    else:
+        print(i)
+
+i2 = iter_b('abcdef')
+c = 1
+for i in i2:
+    if c > 10:
+        break
+    else:
+        print(i)
+        c += 1
