ac78b6733d87c7cb99da6b65950e242cfbbbb88b
RMaturin <maturin@mail.ru>
2021-01-16 19:03:11 +0300

    [upd] Исправил 2 задание в ДЗ урок 4

--- Les04/002.py
+++ Les04/002.py
@@ -4,11 +4,18 @@
 # Результат: [12, 44, 4, 10, 78, 123].
 
 start_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+result_list = [start_list[i] for i in range(1, len(start_list)) if start_list[i-1] < start_list[i]]
+print(start_list)
+print(result_list)
+
+""" Жестко затупил и создал вот такого монстра)) А потом посмотрел разбор домашки
+Я очень долго не мог придумать, как же мне в '... for el in start_list ...' обратиться к предидущему значению списка
 result_list = [
     x[1]
     for x in [(i, el) for i, el in enumerate(start_list) if i > 0]
     for n, y in enumerate(start_list, 1)
     if x[0] == n and x[1] > y
 ]
-print(start_list)
-print(result_list)
+"""
+
+
