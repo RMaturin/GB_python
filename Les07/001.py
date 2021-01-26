6daf4b4928538b65ceeb981f4471029e5cf53e6e
RMaturin <maturin@mail.ru>
2021-01-26 23:29:31 +0300

    [upd] Checkpoint ДЗ урок 7

--- Les07/001.py
+++ Les07/001.py
@@ -12,17 +12,37 @@ class Matrix:
         self.m = len(m_body)
         self.n = max([len(el) for el in m_body])
         # инициализируем матрицу. Если не хватает элементов в строке, добиваем нулями
-        self.body = list()
+        self.__body = list()
+        self.__max_el_width = 0
         for i in range(self.m):
-            self.body.append(list())
+            self.__body.append(list())
             for j in range(self.n):
                 if j < len(m_body[i]):
-                    self.body[i].append(m_body[i][j])
+                    self.__body[i].append(m_body[i][j])
+                    self.__max_el_width = max(self.__max_el_width, len(str(m_body[i][j])))
                 else:
-                    self.body[i].append(0)
+                    self.__body[i].append(0)
+
+    def __str__(self):
+        return '\n'.join([','.join(map(lambda x: str(x).rjust(self.__max_el_width+1, ' '), i)) for i in self.__body])
+
+    def __add__(self, other):
+        if self.m != other.m or self.n != other.n:
+            raise ValueError('Matrices have different sizes')
+        else:
+            result = list()
+            for i in range(self.m):
+                result.append(list())
+                for j in range(self.n):
+                    result[i].append(self.__body[i][j] + other.__body[i][j])
+            return Matrix(result)
 
 
-in_m = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 1, 1, 1]]
-my_m = Matrix(in_m)
-print(my_m.body)
+in_m = [[1, 2, 39], [4, 5, 6], [7, 38, -9], [1, 1, 1]]
 
+my_m1 = Matrix(in_m)
+my_m2 = Matrix(in_m)
+
+r = my_m1 + my_m2 + my_m1
+print(my_m1)
+print(r)
