2c80553079c460a6161ea0f9a481710be6b5ed46
RMaturin <maturin@mail.ru>
2021-01-27 23:03:30 +0300

    [upd] Закончил ДЗ урок 7

--- Les07/002.py
+++ Les07/002.py
@@ -7,3 +7,47 @@
 Реализовать общий подсчет расхода ткани. Проверить на практике полученные на этом уроке знания:
 реализовать абстрактные классы для основных классов проекта, проверить на практике работу декоратора @property."""
 
+from abc import ABC, abstractmethod
+
+
+class Clothes(ABC):
+    def __init__(self, name):
+        self.name = name
+
+    @property
+    @abstractmethod
+    def fabric_size(self):
+        pass
+
+
+class Coat(Clothes):
+    def __init__(self, name, V):
+        super().__init__(name)
+        self.V = int(V)
+
+    @property
+    def fabric_size(self):
+        return round(self.V/6.5 + 0.5, 2)
+
+
+class Suit(Clothes):
+    def __init__(self, name, H):
+        super().__init__(name)
+        self.H = int(H)
+
+    @property
+    def fabric_size(self):
+        return round(2 * self.H + 0.3, 2)
+
+
+c = Coat('Польтишко', 40)
+print(c.name)
+print(c.V)
+print(c.fabric_size)
+
+s = Suit('Костюмчик', 40)
+print(s.name)
+print(s.H)
+print(s.fabric_size)
+
+print(c.fabric_size+s.fabric_size)
